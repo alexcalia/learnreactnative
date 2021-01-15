d58d9c2f54371e164cdff6786743931125091b92
Alex Calia <alex.m.calia@outlook.com>
2021-01-15 12:23:35 -0500

    initial commit, learning React Native

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Here are some boxes of different colours</Text>
+        <View style={[styles.cyanBox, styles.textBox]}>
+          <Text style={styles.boxText}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.blueBox, styles.textBox]}>
+          <Text style={styles.boxText}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.magentaBox, styles.textBox]}>
+          <Text style={styles.boxText}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.orangeBox, styles.textBox]}>
+          <Text style={styles.boxText}>Orange #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 10,
+  },
+  safeArea: {
+    flex: 2,
+  },
+  cyanBox: {
+    backgroundColor: '#2aa198',
+  },
+  textBox: {
+    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  blueBox: {
+    backgroundColor: '#268bd2',
+  },
+  magentaBox: {
+    backgroundColor: '#d33682',
+  },
+  orangeBox: {
+    backgroundColor: '#cb4b16',
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
+
+export default App;
